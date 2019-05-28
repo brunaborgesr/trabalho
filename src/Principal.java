8cdbdeb98e5da73bc308a46b037ba57defe5852e
bruna <bruna@DESKTOP-F48EPTA>
2019-05-27 23:58:47 -0300

    Estrutura modificada

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+    public static void main()
+    {
+        System.out.println("Teste");
+        System.out.println("Bruna");
+        System.out.println("Hozana");
+        System.out.println("Jardel");
+        
+        
+    }
+}
